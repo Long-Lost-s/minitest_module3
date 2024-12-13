d9cab09b1bc04bcb55bbd957905112b41d688d49
Long-Lost-s <hoanglong2117@gmail.com>
2024-12-13 14:51:54 +0700

    students table

---
+++ course.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM student_management.course
+LIMIT 0, 1000
+
+-- Date: 2024-12-13 14:50
+*/
+INSERT INTO `` (`id`,`name`,`description`) VALUES (1,'remember','nhớ');
+INSERT INTO `` (`id`,`name`,`description`) VALUES (2,'understand','hiểu');
+INSERT INTO `` (`id`,`name`,`description`) VALUES (3,'apply','vận dụng');
+INSERT INTO `` (`id`,`name`,`description`) VALUES (4,'analysis','phân tích');
+INSERT INTO `` (`id`,`name`,`description`) VALUES (5,'synthesis','đánh giá');
---
+++ point.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT * FROM student_management.point
+LIMIT 0, 1000
+
+-- Date: 2024-12-13 14:49
+*/
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (1,1,1,10.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (2,1,2,9.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (3,1,3,10.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (4,2,1,8.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (5,2,2,10.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (6,2,3,9.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (7,3,1,7.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (8,3,2,8.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (9,3,3,10.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (10,4,1,8.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (11,4,2,9.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (12,4,3,8.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (13,5,1,7.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (14,5,2,7.00);
+INSERT INTO `` (`id`,`course_id`,`student_id`,`point`) VALUES (15,5,3,7.00);
